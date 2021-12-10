5c9f433304d196c348eb829110969d4f46595b49
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-10 12:47:12 +0300

    chore(eslint): Added ".eslintrc.options.js" file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
-  extends: ["./src/eslintrc"],
-  rules: {
-    "import/no-dynamic-require": "off",
-    "import/no-extraneous-dependencies": [
-      "warn",
-      {
-        bundledDependencies: false,
-        devDependencies: true,
-        optionalDependencies: false,
-        peerDependencies: true
-      }
-    ],
-    "import/no-nodejs-modules": "off"
-  }
+  extends: ["./src/eslintrc"]
 };
