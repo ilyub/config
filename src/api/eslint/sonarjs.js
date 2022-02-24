0eca37252d87de2fc33c9dec02ff1c86225c68a9
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-02-24 20:49:59 +0300

    feat(eslint): Edited "sonarjs/no-identical-functions" rule

--- src/api/eslint/sonarjs.js
+++ src/api/eslint/sonarjs.js
@@ -3,7 +3,7 @@ module.exports = {
   rules: {
     "sonarjs/cognitive-complexity": ["warn", 50],
     "sonarjs/no-duplicate-string": "off",
-    "sonarjs/no-identical-functions": ["warn", 5],
+    "sonarjs/no-identical-functions": ["warn", 10],
     "sonarjs/no-nested-switch": "off",
     "sonarjs/prefer-single-boolean-return": "off"
   }
