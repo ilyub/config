548762fce1c3c2b8dfbcc9ae434efbbd5412ecfe
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-25 23:20:29 +0300

    fix(stylelint): Disabled "font-weight-notation" rule

--- src/stylelint/core.js
+++ src/stylelint/core.js
@@ -60,7 +60,7 @@ module.exports = {
     "font-family-name-quotes": "always-unless-keyword",
     "font-family-no-duplicate-names": true,
     "font-family-no-missing-generic-family-keyword": true,
-    "font-weight-notation": "numeric",
+    "font-weight-notation": undefined,
     "function-allowed-list": [
       "map.get",
       "rotate",
