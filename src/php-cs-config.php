4144ceacd0203a371ba70e7f8f25fc7775259c75
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-04-11 19:28:11 +0300

    feat(php-cs): Added "ordered_class_elements" rule

--- src/php-cs-config.php
+++ src/php-cs-config.php
@@ -4,7 +4,25 @@ class PhpCsConfig
 {
   public static $rules = [
     '@PhpCsFixer' => true,
-    '@Symfony' => true,
+    'ordered_class_elements' => [
+      'order' => [
+        'use_trait',
+        'constant_public',
+        'property_public',
+        'construct',
+        'destruct',
+        'magic',
+        'phpunit',
+        'method_public',
+        'constant_protected',
+        'property_protected',
+        'method_protected',
+        'constant_private',
+        'property_private',
+        'method_private',
+      ],
+      'sort_algorithm' => 'alpha',
+    ],
     'phpdoc_add_missing_param_annotation' => false,
     'yoda_style' => false,
   ];
