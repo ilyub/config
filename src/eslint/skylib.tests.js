971fe3c26b1e614b290d3c97c7d8681db0b68661
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-04-15 00:55:22 +0300

    feat(eslint): Edited "@skylib/..." rules

--- src/eslint/skylib.tests.js
+++ src/eslint/skylib.tests.js
@@ -1,8 +1,10 @@
 module.exports = {
   rules: {
+    "@skylib/no-multi-type-tuples": "off",
     "@skylib/no-mutable-signature": "off",
     "@skylib/no-unsafe-object-assignment": "off",
     "@skylib/prefer-readonly": "off",
+    "@skylib/prefer-readonly-props": "off",
     "@skylib/require-jsdoc": "off"
   }
 };
