c3cb3eae24a84112b9a50de635414fb4591e7ba2
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-06-17 13:54:51 +0300

    feat(api,eslint,boundaries): Edited "createRules" function

--- src/api.js
+++ src/api.js
@@ -2,24 +2,15 @@ module.exports = {
   eslint: {
     boundaries: {
       elementTypes: {
-        createRules: (type, ...args) => {
-          const captures = {};
-
-          for (const [index, arg] of args.slice(0, -1).entries())
-            captures[`part${index + 1}`] = arg;
-
-          const groups = args.at(-1);
-
-          const name = `part${args.length}`;
-
+        createRules: (generator, ...args) => {
           const result = [];
 
-          for (const [index1, group1] of groups.entries())
-            for (const [index2, group2] of groups.entries())
-              if (index1 > index2)
+          for (const [allowIndex, allowFilename] of args.entries())
+            for (const [fromIndex, fromFilename] of args.entries())
+              if (allowIndex < fromIndex)
                 result.push({
-                  allow: [[type, { ...captures, [name]: group2 }]],
-                  from: [[type, { ...captures, [name]: group1 }]]
+                  allow: [generator(allowFilename)],
+                  from: [generator(fromFilename)]
                 });
 
           return result;
