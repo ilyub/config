6e5506f2cbac0100acaf2684feb362f881c5d414
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-24 20:33:37 +0300

    feat(eslint): Edited "@skylib/array-callback-return-type" rule

--- src/eslint/skylib.js
+++ src/eslint/skylib.js
@@ -6,6 +6,7 @@ module.exports = {
     {
       files: "*.js",
       rules: {
+        "@skylib/array-callback-return-type": "off",
         "@skylib/no-mutable-signature": "off",
         "@skylib/no-unsafe-object-assignment": "off",
         "@skylib/prefer-readonly": "off"
