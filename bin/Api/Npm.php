b6b56b240d8af0988450af7317e787ea74405a41
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-30 23:12:37 +0300

    fix(bin): Fixed "noDeprecated" function

--- bin/Api/Npm.php
+++ bin/Api/Npm.php
@@ -88,9 +88,9 @@ class Npm
    */
   public function noDeprecated(): void
   {
-    if (preg_match('`^\d+\.0\.0$`isuxDX', $this->package->version))
+    if (preg_match('`^\d+\.0\.0$`isuxDX', $this->package->version) && file_exists(__DIR__.'/src'))
     {
-      foreach (Sys::scanDirDeep('src') as $path)
+      foreach (Sys::scanDirDeep(__DIR__.'/src') as $path)
       {
         if (is_file($path))
         {
