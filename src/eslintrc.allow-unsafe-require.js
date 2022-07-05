a65e73e0bca59b754de9953b2f3874e94d58d9a8
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-07-05 13:33:23 +0300

    feat(eslint): Edited rules

--- src/eslintrc.allow-unsafe-require.js
+++ src/eslintrc.allow-unsafe-require.js
@@ -1,6 +1,9 @@
 module.exports = {
   rules: {
+    "@typescript-eslint/no-require-imports": "off",
+    "@typescript-eslint/no-var-requires": "off",
     "import/no-dynamic-require": "off",
-    "security/detect-non-literal-require": "off"
+    "security/detect-non-literal-require": "off",
+    "unicorn/prefer-module": "off"
   }
 };
