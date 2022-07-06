77187271c323d7e22b9e3253f26513774c2cd734
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-07-07 02:02:55 +0300

    feat(eslint): Edited .eslintrc.fast.js

--- .eslintrc.fast.js
+++ .eslintrc.fast.js
@@ -1,4 +1,14 @@
 module.exports = {
   extends: ".eslintrc",
-  rules: { "prettier/prettier": "off" }
+  rules: {
+    "@skylib/no-unsafe-object-assignment": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "deprecation/deprecation": "off",
+    "eslint-comments/no-unused-disable": "off",
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/no-internal-modules": "off",
+    "import/no-self-import": "off"
+  }
 };
